6ea7413b4b5b8489163cf639c67d56bd0f43c5f4
Jonathan Beezley <jonathan.beezley@kitware.com>
2016-04-06 13:46:47 -0400

    Checkpoint

--- web_client/js/views/hierarchyWidget.js
+++ web_client/js/views/hierarchyWidget.js
@@ -5,6 +5,71 @@ histomicstk.views.ListView = girder.View.extend({
 });
 
 /**
+ * This is a meta-model that can represent any of the following:
+ *
+ *   * user ->
+ *     * folder[]
+ *     * item[]
+ *   * folder ->
+ *     * folder[]
+ *     * item[]
+ *   * collection ->
+ *     * folder[]
+ *   * item ->
+ *     * file[]
+ *   * file
+ *
+ * It can also represent a few "pseudo" model types that are static
+ * objects displayable in a hierarchy widget but aren't stored
+ * explicitly as a model by girder.
+ *
+ *   * root -> 
+ *     * home (alias to logged in user)
+ *     * users ->
+ *       * user[]
+ *     * collections ->
+ *       * collection[]
+ */
+histomicstk.models.HierarchyModel = girder.Model.extend({
+    initialize: function (attrs, opts) {
+        var triggerAndReturn = _.bind(function (evt) {
+            this.trigger(evt);
+            return this;
+        }, this);
+        var save = triggerAndReturn('g:saved');
+        var fetch = triggerAndReturn('g:fetched');
+        var destroy = triggerAndReturn('g:deleted');
+
+
+        if (!_.has(opts, 'type')) {
+            opts.type = 'root';
+        }
+
+        // set the resource name according to the type
+        this.resourceName = opts.type;
+
+        // override fetch, destroy, and save methods for pseudo types
+        switch(opts.type) {
+            case 'users':
+            case 'home':
+            case 'collections': 
+            case 'root':
+                this.save = save;
+                this.destroy = destroy;
+                this.fetch = fetch;
+                break;
+        }
+    },
+
+    /**
+     * Return a collection that is bound to fetch children of
+     * this model.
+     */
+    children: function () {
+    }
+});
+
+/**
  * Describes an item displayed in the list view.  Must be
  * initialized with a girder ID, model type, and parent ListModel object.
  *
@@ -33,43 +98,44 @@ histomicstk.models.ListModel = Backbone.Model.extend({
         right: null,      // right-aligned text
         badge: null,      // text to display in a bootstrap badge
         tooltip: null,    // text to display in a tooltip on hover
-        icon: null,       // A fontello icon class to display in place of a thumbnail
-
-        // these resources will be loaded on demand
-        meta: null,       // associated metadata displayable in a subview
-        children: null    // a ListCollection of children that can be fetched on demand
+        icon: null        // A fontello icon class to display in place of a thumbnail
     },
 
-    initialize: function (attrs) {
-        attrs = attrs || {};
-
-        if (!attrs.type) { // default root
-            name
-        }
+    initialize: function () {
+        this._girderSync();
     },
 
     /**
      * Load attributes from girder rest calls... dispatches to
-     * the correct method according to the model type.
+     * the correct set* method according to the model type.
      */
     _girderSync: function () {
-        var defer;
+        var defer = $.when(this);
 
         switch(this.get('type')) {
             case 'user':
-                defer = this._girderSyncUser();
+                defer = girder.restRequest({
+                    path: 'user/' + this.id
+                }).then(_.bind(this.setUser, this));
                 break;
             case 'item':
-                defer = this._girderSyncItem();
+                defer = girder.restRequest({
+                    path: 'item/' + this.id
+                }).then(_.bind(this.setItem, this));
                 break;
             case 'collection':
-                defer = this._girderSyncCollection();
+                defer = girder.restRequest({
+                    path: 'collection/' + this.id
+                }).then(_.bind(this.setCollection, this));
                 break;
             case 'users':
-                defer = this._girderSyncUsers();
+                this.setUsers();
                 break;
             case 'collections':
-                defer = this._girderSyncCollections();
+                this.setCollections();
+                break;
+            case null:
+                this.setRoot();
                 break;
             default: // return a rejected promise
                 defer = new $.Deferred().reject('Invalid model type').promise();
@@ -84,12 +150,6 @@ histomicstk.models.ListModel = Backbone.Model.extend({
         );
     },
 
-    _girderSyncUser: function () {
-        return girder.restRequest({
-            path: 'user/' + this.id
-        }).then(_.bind(this.setUser, this));
-    },
-
     /**
      * Set model data from a girder user object.
      */
@@ -99,6 +159,56 @@ histomicstk.models.ListModel = Backbone.Model.extend({
             tooltip: user.login,
             icon: 'icon-user'
         });
+    },
+
+    /**
+     * Set model data from a girder item object.
+     */
+    setItem: function (item) {
+        this.set({
+            name: item.name,
+            tooltip: item.description,
+            icon: 'icon-doc-text-inv'
+        });
+    },
+
+    /**
+     * Set model data from a girder collection object.
+     */
+    setCollection: function (collection) {
+        this.set({
+            name: collection.name,
+            icon: 'icon-box'
+        });
+    },
+
+    /**
+     * Set model data for the psuedo "users" type
+     */
+    setUsers: function () {
+        this.set({
+            name: 'Users',
+            icon: 'icon-users'
+        });
+    },
+
+    /**
+     * Set model data for the psuedo "collections" type
+     */
+    setCollections: function () {
+        this.set({
+            name: 'Collections',
+            icon: 'icon-sitemap'
+        });
+    },
+
+    /**
+     * Set model data for the psuedo "root" type
+     */
+    setRoot: function () {
+        this.set({
+            name: ''
+        });
     }
 });
 
@@ -107,7 +217,8 @@ histomicstk.models.ListModel = Backbone.Model.extend({
  */
 histomicstk.collections.ListCollection = Backbone.Collection.extend({
     model: histomicstk.models.ListModel,
-    localStorage: new Backbone.LocalStorage('HistomicsTK-List-Collection')
+    localStorage: new Backbone.LocalStorage('HistomicsTK-List-Collection'),
+
 });
 
 /**
